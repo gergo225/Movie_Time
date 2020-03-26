f5ec94b0b70a05bd7d63300d7f41fd2df55e29c7
gergo225 <fazekasgergo225@gmail.com>
2020-03-26 13:11:19 +0200

    Add MovieInfoModel to data models and tests for it
    Test: MovieInfoModel extends MovieInfo, fromJson, toJson

---
+++ movie_time/lib/features/movie/data/models/movie_info_model.dart
@@ -0,0 +1,30 @@
+import 'package:movie_time/features/movie/domain/entities/movie_info.dart';
+import 'package:meta/meta.dart';
+
+class MovieInfoModel extends MovieInfo {
+  MovieInfoModel({
+    @required String title,
+    @required int id,
+    @required String releaseDate,
+    @required String overview,
+  }) : super(
+            title: title, id: id, releaseDate: releaseDate, overview: overview);
+
+  factory MovieInfoModel.fromJson(Map<String, dynamic> json) {
+    return MovieInfoModel(
+      title: json["title"],
+      id: json["id"],
+      releaseDate: json["release_date"],
+      overview: json["overview"],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "title": title,
+      "id": id,
+      "release_date": releaseDate,
+      "overview": overview,
+    };
+  }
+}
