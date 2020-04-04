7b400cf8bdeb78ee6253ace1eb6dadb5c54bd435
gergo225 <fazekasgergo225@gmail.com>
2020-04-04 14:29:23 +0300

    Implement and test MovieInfoBloc

--- movie_time/lib/features/movie/presentation/bloc/movie_info_bloc.dart
+++ movie_time/lib/features/movie/presentation/bloc/movie_info_bloc.dart
@@ -1,15 +1,39 @@
 import 'dart:async';
+import 'package:dartz/dartz.dart';
 import 'package:flutter/rendering.dart';
 import 'package:meta/meta.dart';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:movie_time/core/usecases/usecase.dart';
+import '../../../../core/error/failure.dart';
+import '../../../../core/util/input_converter.dart';
+import '../../domain/usecases/get_latest_movie.dart';
+import '../../domain/usecases/get_movie_by_id.dart';
 import '../../domain/entities/movie_info.dart';
 
 part 'movie_info_event.dart';
 part 'movie_info_state.dart';
 
+const String SERVER_FAILURE_MESSAGE = "Server Failure";
+const String CONNECTION_FAILURE_MESSAGE = "Connection Failure";
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    "Invalid Input - The number must be a positive integer";
+
 class MovieInfoBloc extends Bloc<MovieInfoEvent, MovieInfoState> {
+  final GetMovieById getMovieById;
+  final GetLatestMovie getLatestMovie;
+  final InputConverter inputConverter;
+
+  MovieInfoBloc({
+    @required GetMovieById byId,
+    @required GetLatestMovie latest,
+    @required this.inputConverter,
+  })  : assert(byId != null),
+        assert(latest != null),
+        getMovieById = byId,
+        getLatestMovie = latest;
+
   @override
   MovieInfoState get initialState => Empty();
 
@@ -17,6 +41,40 @@ class MovieInfoBloc extends Bloc<MovieInfoEvent, MovieInfoState> {
   Stream<MovieInfoState> mapEventToState(
     MovieInfoEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is GetInfoForMovieById) {
+      final inputEither = inputConverter.stringToUnsignedInt(event.idString);
+      yield* inputEither.fold(
+        (failure) async* {
+          yield Error(message: INVALID_INPUT_FAILURE_MESSAGE);
+        },
+        (integer) async* {
+          yield Loading();
+          final failureOrMovie = await getMovieById(Params(id: integer));
+          yield* _eitherLoadedOrErrorState(failureOrMovie);
+        },
+      );
+    } else if (event is GetInfoForLatestMovie) {
+      yield Loading();
+      final failureOrMovie = await getLatestMovie(NoParams());
+      yield* _eitherLoadedOrErrorState(failureOrMovie);
+    }
+  }
+
+  Stream<MovieInfoState> _eitherLoadedOrErrorState(
+    Either<Failure, MovieInfo> either,
+  ) async* {
+    yield either.fold(
+      (failure) => Error(message: _mapFailureToMessage(failure)),
+      (movie) => Loaded(movie: movie),
+    );
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      default:
+        return "Unexpected Error";
+    }
   }
 }
---
+++ movie_time/test/features/movie/presentation/bloc/movie_info_bloc_test.dart
@@ -0,0 +1,197 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:movie_time/core/error/failure.dart';
+import 'package:movie_time/core/usecases/usecase.dart';
+import 'package:movie_time/core/util/input_converter.dart';
+import 'package:movie_time/features/movie/domain/entities/movie_info.dart';
+import 'package:movie_time/features/movie/domain/usecases/get_latest_movie.dart';
+import 'package:movie_time/features/movie/domain/usecases/get_movie_by_id.dart';
+import 'package:movie_time/features/movie/presentation/bloc/movie_info_bloc.dart';
+
+class MockGetMovieById extends Mock implements GetMovieById {}
+
+class MockGetLatestMovie extends Mock implements GetLatestMovie {}
+
+class MockInputConverter extends Mock implements InputConverter {}
+
+void main() {
+  MovieInfoBloc bloc;
+  MockGetMovieById mockGetMovieById;
+  MockGetLatestMovie mockGetLatestMovie;
+  MockInputConverter mockInputConverter;
+
+  setUp(() {
+    mockGetMovieById = MockGetMovieById();
+    mockGetLatestMovie = MockGetLatestMovie();
+    mockInputConverter = MockInputConverter();
+
+    bloc = MovieInfoBloc(
+      byId: mockGetMovieById,
+      latest: mockGetLatestMovie,
+      inputConverter: mockInputConverter,
+    );
+  });
+
+  test("initial state should be Empty", () {
+    // assert
+    expect(bloc.initialState, equals(Empty()));
+  });
+
+  group("GetInfoForMovieById", () {
+    final idString = "550";
+    // successful output of the InputConverter
+    final idParsed = int.parse(idString);
+    // MovieInfo instance
+    final movieInfo = MovieInfo(
+      title: "Fight Club",
+      id: 550,
+      releaseDate: "1999-10-12",
+      overview:
+          "A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.",
+    );
+
+    void setUpMockInputConverterSuccess() =>
+        when(mockInputConverter.stringToUnsignedInt(any))
+            .thenReturn(Right(idParsed));
+
+    test(
+      "should call the InputConverter to validate and convert the string to an unsigned int",
+      () async {
+        // arrange
+        setUpMockInputConverterSuccess();
+        // act
+        bloc.add(GetInfoForMovieById(idString));
+        await untilCalled(mockInputConverter.stringToUnsignedInt(any));
+        // assert
+        verify(mockInputConverter.stringToUnsignedInt(idString));
+      },
+    );
+
+    test(
+      "should emit [Error] when the input is invalid",
+      () async {
+        // arrange
+        when(mockInputConverter.stringToUnsignedInt(any))
+            .thenReturn(Left(InvalidInputFailure()));
+        // assert later
+        final expected = [
+          // the initial state is always emitted first
+          Empty(),
+          Error(message: INVALID_INPUT_FAILURE_MESSAGE),
+        ];
+        expectLater(bloc, emitsInOrder(expected));
+        // act
+        bloc.add(GetInfoForMovieById(idString));
+      },
+    );
+
+    test(
+      "should get data from the concrete usecase",
+      () async {
+        // arrange
+        setUpMockInputConverterSuccess();
+        when(mockGetMovieById(any)).thenAnswer((_) async => Right(movieInfo));
+        // act
+        bloc.add(GetInfoForMovieById(idString));
+        await untilCalled(mockGetMovieById(any));
+        // assert
+        verify(mockGetMovieById(Params(id: idParsed)));
+      },
+    );
+
+    test(
+      "should emit [Loading, Loaded] when data is gotten successfully",
+      () async {
+        // arrange
+        setUpMockInputConverterSuccess();
+        when(mockGetMovieById(any)).thenAnswer((_) async => Right(movieInfo));
+        // assert later
+        final expected = [
+          Empty(),
+          Loading(),
+          Loaded(movie: movieInfo),
+        ];
+        expectLater(bloc, emitsInOrder(expected));
+        // act
+        bloc.add(GetInfoForMovieById(idString));
+      },
+    );
+
+    test(
+      "should emit [Loading, Error] when getting data fails",
+      () async {
+        // arrange
+        setUpMockInputConverterSuccess();
+        when(mockGetMovieById(any)).thenAnswer((_) async => Left(ServerFailure()));
+        // assert later
+        final expected = [
+          Empty(),
+          Loading(),
+          Error(message: SERVER_FAILURE_MESSAGE),
+        ];
+        expectLater(bloc, emitsInOrder(expected));
+        // act
+        bloc.add(GetInfoForMovieById(idString));
+      },
+    );
+  });
+
+  group("GetInfoForLatestMovie", () {
+    final movieInfo = MovieInfo(
+      title: "Fight Club",
+      id: 550,
+      releaseDate: "1999-10-12",
+      overview:
+          "A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.",
+    );
+
+    test(
+      "should get data from the latest usecase",
+      () async {
+        // arrange
+        when(mockGetLatestMovie(any)).thenAnswer((_) async => Right(movieInfo));
+        // act
+        bloc.add(GetInfoForLatestMovie());
+        await untilCalled(mockGetLatestMovie(any));
+        // assert
+        verify(mockGetLatestMovie(NoParams()));
+      },
+    );
+
+    test(
+      "should emit [Loading, Loaded] when data is gotten successfully",
+      () async {
+        // arrange
+        when(mockGetLatestMovie(any)).thenAnswer((_) async => Right(movieInfo));
+        // assert later
+        final expected = [
+          Empty(),
+          Loading(),
+          Loaded(movie: movieInfo),
+        ];
+        expectLater(bloc, emitsInOrder(expected));
+        // act
+        bloc.add(GetInfoForLatestMovie());
+      },
+    );
+
+    test(
+      "should emit [Loading, Error] when getting data fails",
+      () async {
+        // arrange
+        when(mockGetLatestMovie(any)).thenAnswer((_) async => Left(ServerFailure()));
+        // assert later
+        final expected = [
+          Empty(),
+          Loading(),
+          Error(message: SERVER_FAILURE_MESSAGE),
+        ];
+        expectLater(bloc, emitsInOrder(expected));
+        // act
+        bloc.add(GetInfoForLatestMovie());
+      },
+    );
+  });
+
+}
