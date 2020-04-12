8579109003834207de4d92618c9eb03edc31b0a4
gergo225 <fazekasgergo225@gmail.com>
2020-04-12 16:36:32 +0300

    Implement and test SearchRemoteDataSource

--- movie_time/lib/features/search/data/datasources/search_remote_data_source.dart
+++ movie_time/lib/features/search/data/datasources/search_remote_data_source.dart
@@ -1,8 +1,31 @@
-import 'package:movie_time/features/search/data/models/search_result_model.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
+
+import '../../../../core/error/exception.dart';
+import '../../../../core/util/api_key.dart';
+import '../models/search_result_model.dart';
 
 abstract class SearchRemoteDataSource {
   /// Calls the https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&query={title}&page=1
-  /// 
+  ///
   /// Throws a [ServerException] for all error codes
-  Future<SearchResultModel> searchMovieByTitle(String title); 
+  Future<SearchResultModel> searchMovieByTitle(String title);
+}
+
+class SearchRemoteDataSourceImpl implements SearchRemoteDataSource {
+  final http.Client client;
+
+  SearchRemoteDataSourceImpl({@required this.client});
+
+  @override
+  Future<SearchResultModel> searchMovieByTitle(String title) async {
+    final response = await client.get(
+        "https://api.themoviedb.org/3/search/movie?api_key=$API_KEY&language=en-US&query=$title&page=1");
+    if (response.statusCode == 200) {
+      return SearchResultModel.from(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
