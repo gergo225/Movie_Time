9480502819729f68d01db0e3685bd51623d44f4f
gergo225 <fazekasgergo225@gmail.com>
2020-04-13 19:34:02 +0300

    Update injections to contain the search feature too

--- movie_time/lib/injection_container.dart
+++ movie_time/lib/injection_container.dart
@@ -5,16 +5,33 @@ import 'package:movie_time/features/movie/data/repositories/movie_info_repositor
 import 'package:movie_time/features/movie/domain/repositories/movie_info_repository.dart';
 import 'package:movie_time/features/movie/domain/usecases/get_movie_by_id.dart';
 import 'package:movie_time/features/movie/presentation/bloc/movie_info_bloc.dart';
+import 'package:movie_time/features/search/data/datasources/search_remote_data_source.dart';
+import 'package:movie_time/features/search/data/repositories/search_repository_impl.dart';
+import 'package:movie_time/features/search/domain/usecases/search_movie_by_title.dart';
 
 import 'core/network/network_info.dart';
 import 'core/util/input_converter.dart';
 import 'features/movie/data/datasources/movie_info_remote_data_source.dart';
 import 'features/movie/domain/usecases/get_latest_movie.dart';
+import 'features/search/domain/repositories/search_repository.dart';
+import 'features/search/presentation/bloc/search_bloc.dart';
 
 final sl = GetIt.instance;
 
 void init() {
-  //! Features - Movie Info
+  //! Features - Movie Info, search
+  setUpMovieFeature();
+  setUpSearchFeature();
+
+  //! Core
+  sl.registerLazySingleton(() => InputConverter());
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+  //! External
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => DataConnectionChecker());
+}
+
+void setUpMovieFeature() {
   // Bloc
   sl.registerFactory(
     () => MovieInfoBloc(
@@ -37,10 +54,24 @@ void init() {
   sl.registerLazySingleton<MovieInfoRemoteDataSource>(
     () => MovieInfoRemoteDataSourceImpl(client: sl()),
   );
-  //! Core
-  sl.registerLazySingleton(() => InputConverter());
-  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
-  //! External
-  sl.registerLazySingleton(() => http.Client());
-  sl.registerLazySingleton(() => DataConnectionChecker());
+}
+
+void setUpSearchFeature() {
+  // Bloc
+  sl.registerFactory(() => SearchBloc(
+        byTitle: sl(),
+      ));
+  // Use cases
+  sl.registerLazySingleton(() => SearchMovieByTitle(sl()));
+  // Repository
+  sl.registerLazySingleton<SearchRepository>(
+    () => SearchRepositoryImpl(
+      remoteDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+  // Data sources
+  sl.registerLazySingleton<SearchRemoteDataSource>(
+    () => SearchRemoteDataSourceImpl(client: sl()),
+  );
 }
