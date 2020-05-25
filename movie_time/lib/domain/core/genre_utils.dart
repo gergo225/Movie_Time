084991c7f9c4d26926072f2162b65be8a3054631
gergo225 <fazekasgergo225@gmail.com>
2020-05-25 15:28:49 +0300

    Add id's and names for remaining genres

--- movie_time/lib/domain/core/genre_utils.dart
+++ movie_time/lib/domain/core/genre_utils.dart
@@ -7,8 +7,19 @@ class GenreUtil {
   static int get horror => 27;
   static int get romance => 10749;
   static int get thriller => 53;
+  
+  static int get crime => 80;
+  static int get documentary => 99;
+  static int get family => 10751;
+  static int get fantasy => 14;
+  static int get history => 36;
+  static int get music => 10402;
+  static int get mystery => 9648;
+  static int get scienceFiction => 878;
+  static int get war => 10752;
+  static int get western => 37;
 
-  static get allGenres => [
+  static List<int> get allGenres => [
         action,
         adventure,
         animation,
@@ -24,6 +35,16 @@ class GenreUtil {
     adventure: "Adventure",
     animation: "Animation",
     comedy: "Comedy",
+    crime: "Crime",
+    documentary: "Documentary",
+    family: "Familly",
+    fantasy: "Fantasy",
+    history: "History",
+    music: "Music",
+    mystery: "Mystery",
+    scienceFiction: "Sci-Fi",
+    war: "War",
+    western: "Western",
     drama: "Drama",
     horror: "Horror",
     romance: "Romance",
