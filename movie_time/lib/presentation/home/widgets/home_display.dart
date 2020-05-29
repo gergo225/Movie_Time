a15b3e547b4e0239e57a4a9251635fa26de85f61
gergo225 <fazekasgergo225@gmail.com>
2020-05-29 23:18:46 +0300

    Update home display UI from lists to PageView
    This is still a basic implementation

--- movie_time/lib/presentation/home/widgets/home_display.dart
+++ movie_time/lib/presentation/home/widgets/home_display.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:movie_time/data/home/movie_list_model.dart';
 import 'package:movie_time/domain/home/home_info.dart';
@@ -6,7 +8,7 @@ import 'package:movie_time/domain/home/trending_movie_info.dart';
 import 'package:movie_time/presentation/core/widgets/widgets.dart';
 import 'package:movie_time/presentation/movie/movie_info_page.dart';
 
-class HomeDisplay extends StatelessWidget {
+class HomeDisplay extends StatefulWidget {
   final HomeInfo homeInfo;
 
   const HomeDisplay({Key key, @required this.homeInfo})
@@ -14,159 +16,160 @@ class HomeDisplay extends StatelessWidget {
         super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: Implement home display UI
-    List<MovieListModel<ShortMovieInfo>> genreMovies = [
-      homeInfo.actionMovies,
-      homeInfo.adventureMovies,
-      homeInfo.animationMovies,
-      homeInfo.comedyMovies,
-      homeInfo.dramaMovies,
-      homeInfo.romanceMovies,
-      homeInfo.thrillerMovies,
-      homeInfo.horrorMovies,
-    ];
-    List<Widget> genreMoviesList = List<Widget>.from(
-      genreMovies.map(
-        (genreMovie) => _buildGenreMovies(
-          genreMovie.listName,
-          genreMovie.movieList,
-        ),
-      ),
-    );
+  _HomeDisplayState createState() => _HomeDisplayState();
+}
 
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          _buildTrendingMovies(context),
-          Column(
-            children: genreMoviesList,
-          ),
-        ],
+class _HomeDisplayState extends State<HomeDisplay> {
+  final pageController = PageController(viewportFraction: .9, initialPage: 0);
+  double backgroundLeftOffset = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    List<MovieListModel<ShortMovieInfo>> genreMovies = [
+      widget.homeInfo.actionMovies,
+      widget.homeInfo.adventureMovies,
+      widget.homeInfo.animationMovies,
+      widget.homeInfo.comedyMovies,
+      widget.homeInfo.dramaMovies,
+      widget.homeInfo.romanceMovies,
+      widget.homeInfo.thrillerMovies,
+      widget.homeInfo.horrorMovies,
+    ];
+
+    return NotificationListener(
+      onNotification: (notification) {
+        if (notification is ScrollUpdateNotification) {
+          setState(() {
+            backgroundLeftOffset = -pageController.page * screenWidth;
+          });
+        }
+      },
+      child: Container(
+        child: Stack(
+          children: [
+            Positioned.fill(
+              left: backgroundLeftOffset,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: widget.homeInfo.trendingMovies.movieList.length,
+                itemBuilder: (context, index) {
+                  return PlatformIndependentImage(
+                    imageUrl: widget
+                        .homeInfo.trendingMovies.movieList[index].posterPathUrl,
+                    errorWidget: Container(),
+                    loadingWidget: Container(),
+                    boxFit: BoxFit.cover,
+                    width: screenWidth,
+                  );
+                },
+              ),
+            ),
+            Positioned.fill(
+              top: 32,
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Center(child: _buildTrendingMovies(context)),
+              ),
+            ),
+            Positioned(
+              top: 4,
+              left: 24,
+              child: Text(
+                widget.homeInfo.trendingMovies.listName,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline4
+                    .copyWith(color: Colors.white.withOpacity(.7)),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
   Widget _buildTrendingMovies(BuildContext context) {
-    // TODO: Implement
-    final pageController = PageController(viewportFraction: .8);
-
-    return Column(
-      // crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 12),
-          child: Text(
-            homeInfo.trendingMovies.listName,
-            style: Theme.of(context).textTheme.headline4,
-          ),
-        ),
-        Container(
-          height: 500,
-          child: PageView.builder(
-            controller: pageController,
-            itemCount: homeInfo.trendingMovies.movieList.length,
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) {
-              TrendingMovieInfo movieInfo =
-                  homeInfo.trendingMovies.movieList[index];
-              return GestureDetector(
-                onTap: () => _navigateToMovie(context, movieInfo.id),
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(24),
-                        child: AspectRatio(
-                          aspectRatio: 2 / 3,
-                          child: PlatformIndependentImage(
-                            imageUrl: movieInfo.posterPathUrl,
-                            errorWidget: NoImageWidget.poster(),
-                            loadingWidget: Container(),
-                            boxFit: BoxFit.cover,
-                          ),
-                        ),
+    return Container(
+      // TODO: Figure out height using LayoutBuilder
+      height: 610,
+      child: PageView.builder(
+        controller: pageController,
+        itemCount: widget.homeInfo.trendingMovies.movieList.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          TrendingMovieInfo movieInfo =
+              widget.homeInfo.trendingMovies.movieList[index];
+          return Container(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                GestureDetector(
+                  onTap: () => _navigateToMovie(context, movieInfo.id),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(24),
+                    child: AspectRatio(
+                      aspectRatio: 2 / 3,
+                      child: PlatformIndependentImage(
+                        imageUrl: movieInfo.posterPathUrl,
+                        errorWidget: NoImageWidget.poster(),
+                        loadingWidget: Container(),
+                        boxFit: BoxFit.cover,
                       ),
-                      Text(
-                        movieInfo.title,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(movieInfo.genresString),
-                    ],
+                    ),
                   ),
                 ),
-              );
-            },
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildGenreMovies(String genreName, List<ShortMovieInfo> movies) {
-    // TODO: Implement
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(12),
-          child: Text(
-            genreName,
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
-          ),
-        ),
-        Container(
-          height: 250,
-          child: ListView.separated(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
-            scrollDirection: Axis.horizontal,
-            itemCount: movies.length,
-            separatorBuilder: (context, index) {
-              return SizedBox(width: 8);
-            },
-            itemBuilder: (context, index) {
-              ShortMovieInfo movieInfo = movies[index];
-
-              return GestureDetector(
-                onTap: () => _navigateToMovie(context, movieInfo.id),
-                child: Container(
-                  width: 140,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(12),
-                        child: AspectRatio(
-                          aspectRatio: 2 / 3,
-                          child: PlatformIndependentImage(
-                            imageUrl: movieInfo.posterPathUrl,
-                            errorWidget: NoImageWidget.poster(),
-                            loadingWidget: Container(),
-                            boxFit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Text(
-                        movieInfo.title,
-                        style: TextStyle(fontSize: 16),
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
+                Text(
+                  movieInfo.title,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white.withOpacity(.8),
+                    shadows: [
+                      Shadow(
+                        color: Colors.black,
+                        blurRadius: 4,
+                        offset: Offset(.5, 1),
                       )
                     ],
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              );
-            },
-          ),
-        ),
-      ],
+                Container(
+                  height: 26,
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: movieInfo.genres.length,
+                    separatorBuilder: (context, index) {
+                      return SizedBox(width: 6);
+                    },
+                    itemBuilder: (context, index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          color: Colors.grey.withOpacity(.5),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8),
+                          child: Center(
+                            child: Text(
+                              movieInfo.genres[index],
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 
